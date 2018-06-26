4ff4fc1a673f36a4c0a9e7021b35ec134585fefc
jessica-rmo <jrmo@cin.ufpe.br>
2018-06-26 12:53:10 -0300

    [#6]adicionando passos de teste de preencher formulário

---
+++ tests-acceptance/step-definitions/formulario_nao_membro.ts
@@ -0,0 +1,40 @@
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
+
+let existeEmail = ((elem, email) => elem.element(by.name('listadeemails')).getText().then(text => text === email));
+
+
+defineSupportCode(function ({ Given, When, Then }) {
+	Given(/^eu estou na página "(.*?)"$/, async (pagina) => {
+        await browser.get("http://localhost:4200/");
+        await expect(browser.getTitle()).to.eventually.equal('Recifree');
+        await element(by.linkText(<string> pagina)).click();
+    })
+
+    Given(/^existe formulário com email "(.*?)" na lista de formulários$/, async (email) => {
+        var emails : ElementArrayFinder = element.all(by.name('listadeemails'));
+        var findEmail = emails.filter(elem =>
+                                      elem.getText().then(text => text === email));
+        await findEmail;
+        await findEmail.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    });
+
+    When(/(^eu preencho o campo )?"(.*?)" com "(.*?)"$/, async (campo, info) => {
+        await $("input[name='"+ campo +"']").sendKeys(<string> info);
+    })
+
+    When(/^eu seleciono a opção "(.*?)"$/, async (opcao) => {
+        await element(by.buttonText(<string> opcao)).click();
+    });
+
+    Then(/^eu vejo uma mensagem de erro para "(.*?)"$/, async (msgerror) => {
+    	var msg = element(by.name('msgerro'));
+    	await expect(msg.getText()).toEqual(<string> msgerror); //.toBe
+    })
+
+})
+
