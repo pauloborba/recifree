2b026dd0820ae65499054dc37285bd6d3bd8600b
jessica-rmo <jrmo@cin.ufpe.br>
2018-06-28 00:51:09 -0300

    [FINISHES #6]Completando implementaçao de preencher formulário

--- gui/rd-gui/src/app/app.module.ts
+++ gui/rd-gui/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule }   from '@angular/router';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app.component';
 import { AppHeaderComponent } from './header/app-header.component';
 import { LoginComponent } from './components/login.component';
 import { NoticiasComponent } from './components/noticias.component';
+import { FormulariosComponent } from './components/formularios.component';
+import { FormulariosDataComponent } from './components/formulariosdata.component';
 
 import { NoticiaService } from './components/noticia.service';
+import { FormularioService } from './components/formulario.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
     LoginComponent,
-    NoticiasComponent
+    NoticiasComponent,
+	FormulariosComponent,
+	FormulariosDataComponent
   ],
   imports: [
     BrowserModule,
@@ -27,14 +32,21 @@ import { NoticiaService } from './components/noticia.service';
         path: 'noticias',
         component: NoticiasComponent
       },
+	  {
+		path: 'formularios',
+		component: FormulariosComponent
+	  },
+	  {
+		path: 'formulariosdata',
+		component: FormulariosDataComponent
+	  },
       {
         path: 'login',
         component: LoginComponent
       }
-      
-])
+	])
   ],
-  providers: [NoticiaService],
+  providers: [NoticiaService, FormularioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
