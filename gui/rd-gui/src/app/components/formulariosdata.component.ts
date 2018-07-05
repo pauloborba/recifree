37987188ec4a0559389a7e94e536756e84ad484c
jessica-rmo <jrmo@cin.ufpe.br>
2018-07-05 02:15:09 -0300

    [#9]implementaçao de remover formularios selecionados

--- gui/rd-gui/src/app/components/formulariosdata.component.ts
+++ gui/rd-gui/src/app/components/formulariosdata.component.ts
@@ -5,10 +5,10 @@ import { Formulario } from './formulario';
 import { FormularioService } from './formulario.service';
 
 @Component({
-  selector: 'formulariosdata',
-  templateUrl: './formulariosdata.component.html',
-  styleUrls: ['./formulariosdata.component.css'],
-  providers: [FormularioService]
+	selector: 'formulariosdata',
+	templateUrl: './formulariosdata.component.html',
+	styleUrls: ['./formulariosdata.component.css'],
+	providers: [FormularioService]
 })
 
 export class FormulariosDataComponent implements OnInit {
@@ -16,13 +16,64 @@ export class FormulariosDataComponent implements OnInit {
 	constructor(private formularioService: FormularioService) {}
 	
 	formularios: Formulario[];
+	formRemover: Formulario[] = [];
+
+	onChange(formulario: Formulario) {
+		var checkBox = (<HTMLInputElement> document.getElementById(formulario.email));
+		var statusValue = checkBox.value;
+		checkBox.value = this.changeValue(statusValue);
+
+		if (checkBox.value == 'checked') {
+			this.selectf(formulario);
+		} else {
+			this.deselect(formulario);
+		}
+	}
+
+	changeValue(value: string): string {
+		if (value == 'unchecked') {
+			return 'checked';
+		}
+		return 'unchecked';
+	}
+
+	selectf(formulario: Formulario): void {
+		if (!this.formRemover.includes(formulario)) {
+			this.formRemover.push(formulario);
+		}
+	}
+
+	deselect(formulario: Formulario): void {
+		this.formRemover = this.formRemover.filter(f => f.email !== formulario.email);
+	}
+
+	removerSelecionados(): void {
+		if (this.formRemover.length > 0) {
+			this.hideMessage('msgUnselected');
+			for (let f of this.formRemover) {
+				this.removerFormulario(f);
+			}
+			this.showMessage('msgSuccess');
+			this.formRemover = []; //reiniciando
+		} else {
+			this.showMessage('msgUnselected');
+		}
+	}
 
 	removerFormulario(formulario: Formulario): void {
 		this.formularioService.remover(formulario)
 		.then(formularios => this.formularios = formularios)
 		.catch(erro => alert(erro));
 	}
-		
+
+	hideMessage(id: string): void {
+		(<HTMLInputElement> document.getElementById(id)).hidden = true;
+	}
+
+	showMessage(id: string): void {
+		(<HTMLInputElement> document.getElementById(id)).hidden = false;
+	}
+
 	ngOnInit(): void {
 		this.formularioService.getFormularios()
 		.then(formularios => this.formularios = formularios)
